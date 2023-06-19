eb92b62c335272c614402ed638d389d477336b1a
triantoaldyansyah <antokgodek6@gmail.com>
2023-06-19 17:05:37 +0700

    update architecture and sass concept

---
+++ components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/css/main.css';
+
+export default function Header() {
+    return (
+      <div className={styles.headerContainer}>
+        <h2 className={styles.siteTitle}>DELOSNEWS</h2>
+      </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import styles from '../styles/css/main.css';
+import Header from '../components/header';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Header />
+    </main>
+  )
+}
